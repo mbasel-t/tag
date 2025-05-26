c1b3cbe78e605db172854ed05370cbe6d2aae60f
Matthew Hyatt <mhyatt000@gmail.com>
2025-05-25 22:43:50 -0500

    small changes

--- tag/gym/envs/mixins/cam.py
+++ tag/gym/envs/mixins/cam.py
@@ -11,24 +11,33 @@ import numpy as np
 class CameraMixin:
     """Mixin adding a floating camera and basic controls."""
 
-    floating_camera: gs.Camera | None
+    cam: gs.Camera | None
     _recording: bool
     _recorded_frames: list
 
     def _setup_camera(self) -> None:
         """Create the floating camera from ``self.cfg.viewer``."""
-        self.floating_camera = self.scene.add_camera(
-            res=self.cfg.vis.res,
-            pos=np.array(self.cfg.vis.pos),
-            lookat=np.array(self.cfg.vis.lookat),
-            fov=self.cfg.vis.fov,
-            GUI=not self.cfg.viewer.gui,
-        )
-        self._recording = False
-        self._recorded_frames = []
+        if self.cfg.vis.visualized:
+            self.cam = self.scene.add_camera(
+                res=self.cfg.vis.res,
+                pos=np.array(self.cfg.vis.pos),
+                lookat=np.array(self.cfg.vis.lookat),
+                fov=self.cfg.vis.fov,
+                GUI=not self.cfg.viewer.gui,
+            )
+            self._recording = False
+            self._recorded_frames = []
 
-    def set_camera(self, pos: Tuple[float, float, float], lookat: Tuple[float, float, float]) -> None:
+    def set_camera(
+        self, pos: Optional[Tuple[float, float, float]] = None, lookat: Optional[Tuple[float, float, float]] = None
+    ) -> None:
         """Move the floating camera."""
-        if self.floating_camera is None:
+        if self.cam is None:
             return
-        self.floating_camera.set_pose(pos=pos, lookat=lookat)
+        assert pos is not None or lookat is not None, "Either pos or lookat must be provided"
+        self.cam.set_pose(pos=pos, lookat=lookat)
+
+    def render(self, *args, **kwargs):
+        if self.cfg.vis.visualized:
+            imgs = self.cam.render(*args, **kwargs)
+            return imgs
