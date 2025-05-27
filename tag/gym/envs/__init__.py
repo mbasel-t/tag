fd10884e00003a7c2b21dc0215bac512aee00684
Matthew Hyatt <mhyatt000@gmail.com>
2025-05-27 03:05:57 -0500

    env types

--- tag/gym/envs/__init__.py
+++ tag/gym/envs/__init__.py
@@ -1,2 +1,10 @@
-from ._lr.env import LeggedRobot as LR
-from .chase.chase_env import Chase
+# from ._lr.env import LeggedRobot as LR
+# from chase.chase_env import Chase
+from .base import BaseEnvConfig, BaseEnv
+from .world import WorldEnvConfig, WorldEnv
+from .robotic import RobotEnvConfig, RobotEnv
+
+from typing import Union
+
+ops = (WorldEnvConfig, BaseEnvConfig, RobotEnvConfig)
+EnvTyp = Union[*ops]
