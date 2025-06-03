fde93d9661ab4de6bab3b858d81cca5cdcb58c50
Malec Basel Tarabein <mtarabein@luc.edu>
2025-06-03 14:16:55 -0500

    Basic morphs.Terrain wrapper

--- tag/mbt_testing/TerrainTest.py
+++ tag/mbt_testing/TerrainTest.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass, field
+from typing import Any, Tuple
+
 import genesis as gs
 import numpy as np
+
+import genesis.utils.misc as mu
+from genesis.ext.isaacgym import terrain_utils as isaacgym_terrain_utils
 
 def default(x):
     return field(default_factory=lambda: x)
@@ -27,7 +32,7 @@ class TerrainManager:
 
                 st_types_2d_arr[i][j] = random_terrain
 
-        self.result = gs.morphs.Terrain(
+        self.result = WalledTerrain(
             n_subterrains = (self.n, self.n),
             subterrain_size = (self.size, self.size),
             horizontal_scale = self.horizontal_scale,
@@ -35,10 +40,245 @@ class TerrainManager:
             pos = self.pos,
             subterrain_types = st_types_2d_arr
         )
-        return self.result
+        return self.terrain()
+
+    def add_walls(
+        self,
+        n:int=1,
+        max_n:int=-1,
+        size:int=10,
+        max_size:int=-1,
+        horizontal_only:bool=False,
+        vertical_only:bool=False
+    ): 
+        if (not (horizontal_only and vertical_only)) and (not size < 0) and (not n < 0):
+            self.result.randomize_walls(
+                n=n,
+                max_n=max_n,
+                size=size,
+                max_size=max_size,
+                horizontal_only=horizontal_only,
+                vertical_only=vertical_only
+            )
     
     def terrain(self):
+        return self.result.terrain()
+
+class WalledTerrain:
+    def __init__(self,
+        randomize: bool = False,  # whether to randomize the terrain
+        n_subterrains: Tuple[int, int] = (3, 3),  # number of subterrains in x and y directions
+        subterrain_size: Tuple[float, float] = (12.0, 12.0),  # meter
+        horizontal_scale: float = 0.25,  # meter size of each cell in the subterrain
+        vertical_scale: float = 0.005,  # meter height of each step in the subterrain
+        pos: Tuple[float, float, float] = (0, 0, 0),
+        subterrain_types: Any = "flat_terrain"
+    ):
+        self.randomize = randomize
+        self.n_subterrains = n_subterrains
+        self.subterrain_size = subterrain_size
+        self.horizontal_scale = horizontal_scale
+        self.vertical_scale = vertical_scale
+        self.pos = pos
+        self.subterrain_types = subterrain_types
+        self._post_init()
+
+    def _post_init(self):
+        ################## Validate args ##################
+        supported_subterrain_types = [
+            "flat_terrain",
+            "fractal_terrain",
+            "random_uniform_terrain",
+            "sloped_terrain",
+            "pyramid_sloped_terrain",
+            "discrete_obstacles_terrain",
+            "wave_terrain",
+            "stairs_terrain",
+            "pyramid_stairs_terrain",
+            "stepping_stones_terrain",
+        ]
+
+        if isinstance(self.subterrain_types, str):
+            subterrain_types = []
+            for i in range(self.n_subterrains[0]):
+                row = []
+                for j in range(self.n_subterrains[1]):
+                    row.append(self.subterrain_types)
+                subterrain_types.append(row)
+            self.subterrain_types = subterrain_types
+        else:
+            if np.array(self.subterrain_types).shape != (self.n_subterrains[0], self.n_subterrains[1]):
+                gs.raise_exception(
+                    "`subterrain_types` should be either a string or a 2D list of strings with the same shape as `n_subterrains`."
+                )
+
+        for row in self.subterrain_types:
+            for subterrain_type in row:
+                if subterrain_type not in supported_subterrain_types:
+                    gs.raise_exception(
+                        f"Unsupported subterrain type: {subterrain_type}, should be one of {supported_subterrain_types}"
+                    )
+
+        if not mu.is_approx_multiple(self.subterrain_size[0], self.horizontal_scale) or not mu.is_approx_multiple(
+            self.subterrain_size[1], self.horizontal_scale
+        ):
+            gs.raise_exception("`subterrain_size` should be divisible by `horizontal_scale`.")
+
+
+        ################## Initialization ##################
+        self._init_walls()
+        self._make_heightfield()
+        self._add_walls()
+        self._generate_terrain()
+
+    def _init_walls(self):
+        subterrain_rows = int(self.subterrain_size[0] / self.horizontal_scale)
+        subterrain_cols = int(self.subterrain_size[1] / self.horizontal_scale)
+
+        self.walls = np.zeros(
+            np.array(self.n_subterrains) * np.array([subterrain_rows, subterrain_cols]), dtype=np.int16
+        )
+
+    def _make_heightfield(self):
+        subterrain_rows = int(self.subterrain_size[0] / self.horizontal_scale)
+        subterrain_cols = int(self.subterrain_size[1] / self.horizontal_scale)
+
+        heightfield = np.zeros(
+            np.array(self.n_subterrains) * np.array([subterrain_rows, subterrain_cols]), dtype=np.int16
+        )
+
+        for i in range(self.n_subterrains[0]):
+            for j in range(self.n_subterrains[1]):
+                subterrain_type = self.subterrain_types[i][j]
+
+                new_subterrain = isaacgym_terrain_utils.SubTerrain(
+                    width=subterrain_rows,
+                    length=subterrain_cols,
+                    vertical_scale= self.vertical_scale,
+                    horizontal_scale= self.horizontal_scale,
+                )
+                if not self.randomize:
+                    saved_state = np.random.get_state()
+                    np.random.seed(0)
+
+                if subterrain_type == "flat_terrain":
+                    subterrain_height_field = np.zeros((subterrain_rows, subterrain_cols), dtype=np.int16)
+
+                elif subterrain_type == "fractal_terrain":
+                    subterrain_height_field = fractal_terrain(new_subterrain, levels=8, scale=5.0).height_field_raw
+
+                elif subterrain_type == "random_uniform_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.random_uniform_terrain(
+                        new_subterrain,
+                        min_height=-0.1,
+                        max_height=0.1,
+                        step=0.1,
+                        downsampled_scale=0.5,
+                    ).height_field_raw
+
+                elif subterrain_type == "sloped_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.sloped_terrain(
+                        new_subterrain,
+                        slope=-0.5,
+                    ).height_field_raw
+
+                elif subterrain_type == "pyramid_sloped_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.pyramid_sloped_terrain(
+                        new_subterrain,
+                        slope=-0.1,
+                    ).height_field_raw
+
+                elif subterrain_type == "discrete_obstacles_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.discrete_obstacles_terrain(
+                        new_subterrain,
+                        max_height=0.05,
+                        min_size=1.0,
+                        max_size=5.0,
+                        num_rects=20,
+                    ).height_field_raw
+
+                elif subterrain_type == "wave_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.wave_terrain(
+                        new_subterrain,
+                        num_waves=2.0,
+                        amplitude=0.1,
+                    ).height_field_raw
+
+                elif subterrain_type == "stairs_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.stairs_terrain(
+                        new_subterrain,
+                        step_width=0.75,
+                        step_height=-0.1,
+                    ).height_field_raw
+
+                elif subterrain_type == "pyramid_stairs_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.pyramid_stairs_terrain(
+                        new_subterrain,
+                        step_width=0.75,
+                        step_height=-0.1,
+                    ).height_field_raw
+
+                elif subterrain_type == "stepping_stones_terrain":
+                    subterrain_height_field = isaacgym_terrain_utils.stepping_stones_terrain(
+                        new_subterrain,
+                        stone_size=1.0,
+                        stone_distance=0.25,
+                        max_height=0.2,
+                        platform_size=0.0,
+                    ).height_field_raw
+
+                else:
+                    gs.raise_exception(f"Unsupported subterrain type: {subterrain_type}")
+
+                if not self.randomize:
+                    np.random.set_state(saved_state)
+
+                heightfield[
+                    i * subterrain_rows : (i + 1) * subterrain_rows, j * subterrain_cols : (j + 1) * subterrain_cols
+                ] = subterrain_height_field
+
+        self.base_heightfield = heightfield
+    
+    def _add_walls(self):
+        subterrain_rows = int(self.subterrain_size[0] / self.horizontal_scale)
+        subterrain_cols = int(self.subterrain_size[1] / self.horizontal_scale)
+
+        heightfield = np.zeros(
+            np.array(self.n_subterrains) * np.array([subterrain_rows, subterrain_cols]), dtype=np.int16
+        )
+
+        for x in range(len(heightfield)):
+            for y in range(len(heightfield[x])):
+                heightfield[x,y] = self.base_heightfield[x,y] + self.walls[x,y]
+
+        self.heightfield = heightfield
+
+    def _generate_terrain(self):
+        self.result = gs.morphs.Terrain(
+            horizontal_scale= self.horizontal_scale,
+            vertical_scale= self.vertical_scale,
+            pos= self.pos,
+            height_field= self.heightfield
+        )
+
+    def randomize_walls(self, n, max_n, size, max_size, horizontal_only, vertical_only):
+        # TODO: add randomization to ts
+
+        # sample
+        for x in range(len(self.walls)):
+            self.walls[x,0] = 70
+
+        self._add_walls()
+        self._generate_terrain()
+
+    def terrain(self):
         return self.result
+
+    def phf(self): #debug
+        for row in self.heightfield:
+            print(row)
+        quit()
+
 
 
 ################### SAMPLE CODE ###################
@@ -69,7 +309,7 @@ def main():
         gs.morphs.Plane()
     )
 
-    n, size, z_off = 1, 5.0, 10
+    n, size, z_off = 2, 5.0, 10
     # terrain = scene.add_entity(
     #     gs.morphs.Terrain(
     #         n_subterrains=(n,n),
@@ -89,6 +329,8 @@ def main():
     test_terrain = TerrainManager(
         n, size, z_off, 0.05, 0.01, subterrain_types
     )
+
+    test_terrain.add_walls()
 
     terrain = scene.add_entity(
         test_terrain.terrain()
